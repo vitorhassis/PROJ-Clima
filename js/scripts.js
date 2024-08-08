4c998667355bc9b39d0862c56b67deeddc4e9875
Vitor Assis <vitorassis997@gmail.com>
2024-08-08 15:34:10 -0300

    update projeto clima api

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,9 @@ const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
-
+const weatherContainer = document.querySelector("#weather-data");
+const errorMessageContainer = document.querySelector("#error-message");
+const messageh3 = document.querySelector("#h3")
 //funções
 
 //funcao que acessa a API
@@ -25,16 +27,40 @@ const getWeatherData = async (city) => {
   return data;
 };
 
-const showWeatherData = async (city) => {
-    const data = await getWeatherData(city);
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
 
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp); //as vezes a temp vem com info quebrada
-    descElement.innerText = data.weather[0].description; //vai ser a descricao (?)
-    weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
-    countryElement.setAttribute("src", `https://flagcdn.com/16x12/${data.sys.country.toLowerCase()}.png`);
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`;
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+}
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if(data.cod === "404") {
+    showErrorMessage();
+    return
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp); //as vezes a temp vem com info quebrada
+  descElement.innerText = data.weather[0].description; //vai ser a descricao (?)
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute(
+    "src",
+    `https://flagcdn.com/16x12/${data.sys.country.toLowerCase()}.png`
+  );
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  weatherContainer.classList.remove("hide"); //para remover a classe "hide" quando apertar na lupa, qnd ocorrer a pesquisa da cidade
 };
 
 //eventos
@@ -44,4 +70,14 @@ searchBtn.addEventListener("click", async (e) => {
   const city = cityInput.value;
 
   showWeatherData(city);
+
+  h3.textContent = 'Confira o clima desta cidade:';
+  
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value
+    showWeatherData(city)
+  }
 });
